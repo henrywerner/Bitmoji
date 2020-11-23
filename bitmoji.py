ee71d2d2174802ac6db20614bf8bbf59d06007fa
quaintt <htwerner01@gmail.com>
2020-11-22 21:54:37 -0600

    spell checked

--- bitmoji.py
+++ bitmoji.py
@@ -1,26 +1,25 @@
 import json
 import urllib.request
 
-# Discover, construct, and save bitmoji panels within the given catagory
-def get_panels(catagory):
-    t_ids = [] # template IDs
-    t_urls = [] # template URLs
+# Discover, construct, and save bitmoji panels within the given category
+def get_panels(category):
+    panels = []
     
     # has_friends boolean used in url assembly
     has_friends = False
-    if (catagory == 'friends'):
+    if (category == 'friends'):
         has_friends = True
     
-    # find items in given catagory
-    for item in templates[catagory]:
+    # find items in given category
+    for item in templates[category]:
         if search_tag in item['tags']:
-            t_ids.append(item['template_id'])
-            t_urls.append(item['src'])
+            panels.append(item)
     
     # loop through discovered comics
-    name_counter = 0
-    for t in t_urls:
-        index1 = t.find("%s") # only first index is needed
+    counter = 0
+    for x in panels:
+        src = x.get("src")
+        index1 = src.find("%s") # only first index is needed
         index2 = index1 + 2
 
         if has_friends:
@@ -29,21 +28,22 @@ def get_panels(catagory):
         else:
             insert = userID[0]
 
-        url = t[0:int(index1)] + insert + t[int(index2):] # replace userIDs with insert
-        filename = t_ids[name_counter] + ".png" # build filename using comic ID
+        url = src[0:int(index1)] + insert + src[int(index2):] # replace userIDs with insert
+        filename = x.get("template_id") + ".png" # build filename using comic ID
         urllib.request.urlretrieve(url, filename) # save png from url
-        name_counter += 1
+        counter += 1
 
-        print("[" + str(name_counter) + "]: " + url)
-
-    # clear arrays in case of additional calls
-    t_ids.clear()
-    t_urls.clear()
-
-    if (name_counter == 0):
-        print('No templates found.')
-    else:
-        print('Templates found: ' + str(name_counter))
+        # This code sucks. I hate it. I'll fix it later.
+        print("[" + str(counter) + "]\tid: " + x.get("template_id"), end = "\t")
+        j = len(x["tags"]) - 1
+        start_text = " "
+        tags = "tags:"
+        k = 0
+        while (k < j and k < 3):
+            tags += start_text + x["tags"][k]
+            start_text = ", "
+            k += 1
+        print(tags)
 
 
 ####  Initial Set Up  ####
@@ -71,8 +71,6 @@ f.close()
 
 # call appropriate method based on number of provided userIDs
 if (len(userID) > 1):
-    #get_friendmojis()
     get_panels('friends')
 else:
-    #get_imoji()
     get_panels('imoji')
